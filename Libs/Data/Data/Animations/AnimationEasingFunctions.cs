35657d3cd92f60f266ba6d90708f8a9ca13ec5c1
Matheus Moreira <matheusmatos32@gmail.com>
2022-02-27 00:58:14 -0300

    Raised animation easing exponent / Better easing formula

--- Libs/Data/Data/Animations/AnimationEasingFunctions.cs
+++ Libs/Data/Data/Animations/AnimationEasingFunctions.cs
@@ -3,13 +3,18 @@ namespace OpenTeleprompter.Data.Animations
 {
     public static class AnimationEasingFunctions
     {
+        public const double EASING_EXPONENT = 5;
+
         public static readonly AnimationEasingFunction Linear = (double x) => x;
 
         public static readonly AnimationEasingFunction EaseIn = (double x) =>
-            x < .5 ? 4 * Math.Pow(x, 3) :  1 - 4 * Math.Pow(1 - x, 3);
+            x < .5 ? Math.Pow(2 * x, EASING_EXPONENT) / 2 :
+                1 - Math.Pow(2 * (1 - x), EASING_EXPONENT) / 2;
 
-        public static readonly AnimationEasingFunction EaseOut = (double x) => Math.Pow(x, 3);
+        public static readonly AnimationEasingFunction EaseOut = (double x) =>
+            Math.Pow(x, EASING_EXPONENT);
 
-        public static readonly AnimationEasingFunction Ease = (double x) => 1 - Math.Pow(1 - x, 3);
+        public static readonly AnimationEasingFunction Ease = (double x) =>
+            1 - Math.Pow(1 - x, EASING_EXPONENT);
     }
 }
